2e986c3ae0ac1b9d1079dc357d54064b2312e78c
JadonPage <jadontpage@outlook.com>
2022-12-29 16:25:16 -0600

    Commit Day 16 files

---
+++ Coding Exercises/Day 16/Coding Exercise 1.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+feet_label = sg.Text("Enter feet:")
+feet_input = sg.InputText(tooltip="Enter feet")
+
+inches_label = sg.Text("Enter inches:")
+inches_input = sg.InputText(tooltip="Enter inches")
+
+convert_button = sg.Button("Convert")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [convert_button]])
+
+window.read()
+window.close()
