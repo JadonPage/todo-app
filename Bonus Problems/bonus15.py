a95b3b523092556c9c1249f03a093b61e15b5d22
JadonPage <jadontpage@outlook.com>
2022-12-29 15:55:55 -0600

    Commit Bonus Files

---
+++ Bonus Problems/bonus15.py
@@ -0,0 +1,32 @@
+import json
+
+with open("Files/bonus15questions.json", 'r') as file:
+    content = file.read()
+
+data = json.loads(content)
+
+for question in data:
+    print(question["question_text"])
+    for index, alternative in enumerate(question["alternatives"]):
+        index += 1
+        print(f"{index}. {alternative}")
+    user_choice = int(input("Enter your answer: "))
+    question["user_choice"] = user_choice
+
+
+score = 0
+
+for index, question in enumerate(data):
+    index += 1
+    if question["user_choice"] == question["correct_answer"]:
+        score += 1
+        result = "Correct Answer"
+    else:
+        result = "Wrong Answer"
+
+    message = f"{index} {result}. Your answer: {question['user_choice']}," \
+              f"Correct answer: {question['correct_answer']}"
+    print(message)
+
+
+print(score, "/", len(data))
